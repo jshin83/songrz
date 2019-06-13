abe38794fe092e60a8f55e0c513a00a67cc47049
jshin83 <jen.kyongah.lee@gmail.com>
2019-06-13 14:46:01 -0700

    shows user if there are no albums to show, conditional in html

--- src/main/java/edu/jenshin831/lab/songr/AlbumController.java
+++ src/main/java/edu/jenshin831/lab/songr/AlbumController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Collection;
+
 @Controller
 public class AlbumController {
 
